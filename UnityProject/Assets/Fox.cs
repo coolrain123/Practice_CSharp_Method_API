284d4187017147dffa660f4c0f8a8df79a73c32c
coolrain123 <59252383+coolrain123@users.noreply.github.com>
2020-01-14 13:55:56 +0800

    狐狸移動

---
+++ UnityProject/Assets/Fox.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fox : MonoBehaviour
+{
+    [Header("移動速度"), Range(0f, 100f)]
+    public float speed = 1.5f;
+    [Header("剛體")]
+    public Rigidbody2D rig;
+    [Header("Sr")]
+    public SpriteRenderer Sr;
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+   
+    private void FixedUpdate()
+    {
+        Move();
+    }   
+
+    private void Move()
+    {
+       
+        rig.AddForce(transform.right * Input.GetAxisRaw("Horizontal") * speed);
+        if (Input.GetKeyDown(KeyCode.A)) { Sr.flipX = true; }
+        if (Input.GetKeyDown(KeyCode.LeftArrow)) { Sr.flipX = true; }
+        if (Input.GetKeyDown(KeyCode.D)) { Sr.flipX = false; }
+        if (Input.GetKeyDown(KeyCode.RightArrow)) { Sr.flipX = false; }
+    }
+
+
+
+
+
+}
